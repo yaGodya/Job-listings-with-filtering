5a3194ba2c7d62c2e1860ae13d1863fb439b7b03
yaGodya <krispygeorg@gmail.com>
2025-03-13 01:01:01 +0200

    Add filters

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,22 +1,41 @@
-import { Badge } from 'UI/Badge';
-import { Card } from 'UI/Card';
-import { Stack } from 'UI/Stack';
+import { useSelector, useDispatch } from "react-redux";
 
+import { Badge } from "UI/Badge";
+import { Card } from "UI/Card";
+import { Stack } from "UI/Stack";
+
+import { selectFilters } from "store/filters/filter-selectors";
+import { removeFilter, clearFilter } from "store/filters/filter-actions";
 
 const FilterPanel = () => {
-  return (
-    <Card className="filter-panel">
-      <div className="filter-panel-wrapper">
-        <Stack>
-          <Badge variant="clearable">Frontend</Badge>
-          <Badge variant="clearable">Backend</Badge>
-          <Badge variant="clearable">React</Badge>
-        </Stack>
+    const currentFilters = useSelector(selectFilters);
+    const dispatch = useDispatch();
 
-        <button className='link'>Clear</button>
-      </div>
-    </Card>
-  )
-}
+    if (currentFilters.length === 0) {
+        return null;
+    }
 
-export {FilterPanel};
+    return (
+        <Card className="filter-panel">
+            <div className="filter-panel-wrapper">
+                <Stack>
+                    {currentFilters.map((filter) => (
+                        <Badge
+                            key={filter}
+                            variant="clearable"
+                            onClear={() => dispatch(removeFilter(filter))}
+                        >
+                            {filter}
+                        </Badge>
+                    ))}
+                </Stack>
+
+                <button onClick={() => dispatch(clearFilter)} className="link">
+                    Clear
+                </button>
+            </div>
+        </Card>
+    );
+};
+
+export { FilterPanel };
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import { positinReducer } from "./positions/position-reducer";
+import {filterReducer} from './filters/filter-reducer'
 
 export const rootReducer = combineReducers({
     positions: positinReducer,
+    filters: filterReducer,
 });
