2094cbbdabfdd774c6fc4716d7105f7294dcecf8
yaGodya <krispygeorg@gmail.com>
2025-03-14 00:50:51 +0200

    add persistor to save in local-storage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "store";
 
-import { store } from 'store';
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
         </Provider>
     </React.StrictMode>,
     document.getElementById("root")
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,22 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
+const persistConfig = {
+    key: "root",
+    storage,
+    // whiteList: ['filters', 'positions'],
+    // blackList: [],
+};
+
+const persistedReducer =  persistReducer(persistConfig, rootReducer)
+
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     window.__REDUX_DEVTOOLS_EXTENTION__ && window.__REDUX_DEVTOOLS_EXTENTION__()
 );
 
 export { store };
+  export const persistor = persistStore(store)
